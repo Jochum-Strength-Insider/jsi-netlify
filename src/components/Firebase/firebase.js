ade1d612566e83ad6a579708c8d735d167823aa4
seanneppl <sean.e.neppl@gmail.com>
2020-03-08 22:31:05 -0500

    removed log

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -13,14 +13,6 @@ const config = {
    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
-console.log(process.env.REACT_APP_API_KEY);
-console.log(process.env.REACT_APP_AUTH_DOMAIN);
-console.log(process.env.REACT_APP_DATABASE_URL);
-console.log(process.env.REACT_APP_PROJECT_ID);
-console.log(process.env.REACT_APP_STORAGE_BUCKET);
-console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
-
-
 // const config = {
 //    apiKey: "AIzaSyDL3ov_61hBbG4S7YxUd6GJ9ZjdSA2fByo",
 //    authDomain: "auth-practice-23bfe.firebaseapp.com",
