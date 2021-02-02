816d1ef9f761154b30e0df870795d247a9739911
seanneppl <sean.e.neppl@gmail.com>
2021-02-01 18:39:17 -0600

    quick test

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-export const SIGN_UP = '/signuptest';
+export const SIGN_UP = '/signup';
 export const INSIDER = '/insider-signup';
 export const SIGN_IN = '/signin';
 export const SIGN_IN_LINK = '/email-signin';
